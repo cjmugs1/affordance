8455fffc0dc28422ed5ed0cf8331620aba8b8ee0
Connor Mulligan <connor26m@gmail.com>
2023-04-19 23:12:31 -0600

    trying to add static.json so that routes render on heroku

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import Resume from './pages/Resume';
 function App() {
   return (
     <Router>
-        <div className="app-parent-div">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/work" element={<Work />} />
-            <Route path="/contact" element={<Contact />} />
-            {/* <Route path="/resume" element={<Resume />} /> */}
-          </Routes>
-        </div>
-      </Router>
+      <div className="app-parent-div">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/resume" element={<Resume />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
